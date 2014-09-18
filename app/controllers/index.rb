02001d3180b1603b38086a2aafccc2fd3ab81a14
Joan Torres & Melissa Porto <pair+Gunga.melissaporto@devbootcamp.com>
2014-09-18 12:12:07 -0700

    ADD BASIC CSS
    We added the intial styling to the website and created some of the views.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,8 @@
 get '/' do
   # Look in app/views/index.erb
-  erb :index
+  if session[:user_id]==nil
+    erb :welcome
+  else
+    erb :home
+  end
 end
---
+++ app/helpers/helpers.rb
@@ -0,0 +1,14 @@
+helpers do
+  def nav
+    <<-NAVIGATION
+<nav>
+  <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Profile</a></li>
+  </ul>
+
+  <h1>OINKER</h1>
+</nav>
+    NAVIGATION
+  end
+end
